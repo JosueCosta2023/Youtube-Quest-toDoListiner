17fec48d885333c863c6befc5ae189c9ab595532
Josue Costa <contato_josuecosta@hotmail.com>
2023-09-22 11:57:57 -0400

    Atualizacao 2209AM

--- script.js
+++ script.js
@@ -28,10 +28,9 @@ const handleAddTask = () => {
 
     const taskContent = document.createElement('p')
 
-    //Adicionando monitoramento no paragrafo, ele ira chamar a função handleClick quando clicado
-    // taskContent.addEventListener('click', () => handleClick(taskContent));
-
-    taskContent.addEventListener("click", () => handleClick(taskContent));
+        //Adicionando monitoramento no paragrafo, ele ira chamar a função handleClick quando clicado
+        // taskContent.addEventListener('click', () => handleClick(taskContent));
+        taskContent.addEventListener("click", () => handleClick(taskContent));
 
 
 
@@ -41,8 +40,10 @@ const handleAddTask = () => {
     deleteItem.classList.add("far");
     deleteItem.classList.add("fa-trash-alt");
 
-    //Adicionando evento no botao delete, ele ira chamar a função excluir quando clicado
-    deleteItem.addEventListener('click', () => handleDeleteClick());
+        //Adicionando evento no botao delete, ele ira chamar a função excluir quando clicado
+        deleteItem.addEventListener('click', () => handleDeleteClick(
+            taskItemContainer, taskContent
+        ));
     
     //Adicionando o valor do input no item da lista de tarefas
     taskContent.innerText = inputElement.value;
@@ -57,6 +58,8 @@ const handleAddTask = () => {
 
     inputElement.value = "";
 
+    // updateLocalStorage();
+
 };
 
  //Função que risca o paragrafo clicado, deixado ele com status de concluido.
@@ -68,13 +71,32 @@ const handleAddTask = () => {
      //O FOR ira percorre a lista de tarefa tasks e inserir o valor na task
      //a fim de validar se o valor corresponde a tarefa clicada
      for (const task of tasks) {
-         //firstChild = é a classe p
+        //Variavel de verificação para o if
+        const currentTaskIsBeginClicked = task.firstChild.isSameNode(taskContent);
+         
+        //firstChild = é a classe p
          //isSomeNode  = é a mesma coisa que outra
-         if (task.firstChild.isSameNode(taskContent)) {
+         if (currentTaskIsBeginClicked) {
              //Se cair nesse if então atualizamos o text decoration
              task.firstChild.classList.toggle("completed");
          }
      }
+
+    //  updateLocalStorage();
+ }
+
+ //ARROW-FUNCTION
+ const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks) {
+        const currentTaskIsBeginClicked = task.firstChild.isSameNode(taskContent);
+        if(currentTaskIsBeginClicked){
+            taskItemContainer.remove();
+        }
+    }
+
+    // updateLocalStorage();
  }
 
 
@@ -86,9 +108,56 @@ const handleInputChange = () => {
     if (inputIsValid) {
         return inputElement.classList.remove('error');
     }
+
+    // updateLocalStorage();
     
 };
 
+// //Guardando os dados no localstorage
+// const updateLocalStorage = () =>{
+//     const tasks = tasksContainer.childNodes;
+
+//     const localStorageTasks = [...tasks].map((task) => {
+//         const content = task.firstChild;
+//         const isCompleted = content.classList.contains("completed");
+
+//         return {description: content.innerText, isCompleted};
+//     });
+
+//     localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+// };
+
+// //Função para recarregar as tarefas
+// const refreshTasksUsingLocalStorage = () => {
+
+//     const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+
+//     if (!tasksFromLocalStorage) return;
+
+//     for (const task of tasksFromLocalStorage) {
+
+//         const taskItemContainer = document.createElement('div');
+//         taskItemContainer.classList.add("task-item");
+
+//         const taskContent = document.createElement('p');
+//         taskContent.innerHTML = task.description;
+
+//         if (task.isCompleted) {
+//             taskContent.classList.add("completed");
+//         };
+
+//         taskContent.addEventListener("click", () => handleClick(taskContent));
+
+//         const deleteItem = document.createElement("i");
+//         deleteItem.classList.add("far");
+//         deleteItem.classList.add("fa-trash-alt");
+
+//         tasksContainer.appendChild(taskItemContainer);
+
+//     }
+// };
+// //Recarrega e tras os dados ja inseridos
+// refreshTasksUsingLocalStorage();
 
 //Evento do botao, adicionar tarefa
 addTaskButton.addEventListener('click', () => handleAddTask());
